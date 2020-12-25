29ac25baad08613a9439f1403a459d1b55b886ab
Admin <Admin@sandhya>
2020-12-25 21:00:50 +0530

    sandhya commit (Customer master updated)

--- src/main/java/com/capgemini/advertisement/entity/CustomerMaster.java
+++ src/main/java/com/capgemini/advertisement/entity/CustomerMaster.java
@@ -1,8 +1,12 @@
 package com.capgemini.advertisement.entity;
 
+ 
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+
+ 
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,8 +15,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+ 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+ 
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,31 +36,56 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+ 
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Table(name="customer_master")
 public class CustomerMaster {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cust_id")
-	private Integer custId;
+    private Integer custId;
+    
+    @NotNull
+    @NotBlank(message="Please enter your name")
+    @Size(min=2, max=30,message="First name should have atleast 2 characters")
    @Column(name = "cust_first_name")
-	private String custFirstName;
+    private String custFirstName;
+    
+    @NotNull
+    @NotBlank(message="Please enter your name")
+    @Size(min=2, max=30,message="Last name should have atleast 2 characters")
     @Column(name = "cust_last_name")
-	private String custLastName;
+    private String custLastName;
+    
+    @NotNull
+    @NotBlank(message="Please enter your email")
+    @Email(message = "Email should be valid")
+    @Pattern(regexp="[A-Za-z]+[0-9]*@[a-zA-Z]+.[a-zA-A]+")
     @Column(name = "cust_email")
-	private String custEmail;
+    private String custEmail;
+    
+    @NotNull
+    @NotBlank(message="Please enter your phone number")
+    @Pattern(regexp="([7-9][0-9]{9})")
     @Column(name = "cust_mobile")
-	private String custMobile;
+    private String custMobile;
+    
+    @NotNull
+    @NotBlank(message="Please enter password")
    @Column(name = "cust_password")
-	private String custPassword;
-	
+    private String custPassword;
+    
    @ToString.Exclude
    @JsonIgnore
-	@OneToMany(mappedBy = "customer")
-	private Set<AdvertisementDetails> advertisement;
+    @OneToMany(mappedBy = "customer")
+    private Set<AdvertisementDetails> advertisement;
+
+ 
 
 
 }
+ 
