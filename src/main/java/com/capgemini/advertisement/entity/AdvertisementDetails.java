4147cd0300ec04ea143a9a01df51cb8c59c45197
Sapna <Sapna@LAPTOP-TC441QP5>
2020-12-26 21:08:24 +0530

    Sapna's commit for advertisement

--- src/main/java/com/capgemini/advertisement/entity/AdvertisementDetails.java
+++ src/main/java/com/capgemini/advertisement/entity/AdvertisementDetails.java
@@ -1,11 +1,6 @@
 package com.capgemini.advertisement.entity;
 
-
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,21 +11,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 import org.hibernate.annotations.Type;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
-
+/**
+ * 
+ * @author Sapna and Dakshata
+ *
+ */
+//This annotation is used to create and declare the table name
 @Entity
 @Table(name="advertisement_details")
 @Data
@@ -41,46 +35,40 @@ public class AdvertisementDetails {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private Integer id;
-	
+
 	@Column(name="adv_type" ,nullable=false)
 	private String advType;
-	
+
 	@Column(name="created_by")
 	private String createdBy;
-	
-	
+
+
 	@Column(name="adv_location")
 	private String advLocation;
-	
+
 	@Column(name="start_date")
 	private LocalDate startDate;
-	
+
 	@Column(name="end_date")
 	private LocalDate endDate;
-	
+
 	@Lob
 	@Type(type="org.hibernate.type.BinaryType")
 	@Column(name="adv_image")
 	private byte[] advImage;
-	
+
 	@ToString.Exclude
 	@JsonIgnore
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "cust_id")
 	private CustomerMaster customer;
-	
+
 	@ToString.Exclude
 	@JsonIgnore
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "staff_id")
 	private Staff staff;
-	
+
 
 
 }
-
-
-
-
-
-
